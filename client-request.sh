6a7c1e7d93aa1457bd54171fd4f579f59392ec76
Nathan Phillip Brink <binki@gentoo.org>
2017-04-20 07:39:47 +0000

    Update deployment URI temporarily

--- client-request.sh
+++ client-request.sh
@@ -3,4 +3,4 @@ if ! [ -e "${1}" ]; then
     echo "Usage: ${0} /path/to/new/certificate.pem" >&2
     exit 1
 fi
-curl --cert anarchyirc-client-key.crt --key anarchyirc-client-key.key --data-urlencode cert@"${1}" https://sam.ohnopub.net/~binki/irc-poc/index.cgi/update
+curl --cert anarchyirc-client-key.crt --key anarchyirc-client-key.key --data-urlencode cert@"${1}" https://sam.ohnopub.net/~binki/anarchyirc-link-block-service/index.cgi/update
