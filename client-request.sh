b2a0f83c70660f3b3dd747834cc1a6f996b3c057
Nathan Phillip Brink <binki@gentoo.org>
2017-05-10 03:31:02 +0000

    Modify request script to accept endpoint as a parameter.

--- client-request.sh
+++ client-request.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if ! [ -e "${1}" ]; then
-    echo "Usage: ${0} /path/to/new/certificate.pem" >&2
+if ! [ -e "${2}" ]; then
+    echo "Usage: ${0} https://example.org/endpoint /path/to/new/certificate.pem" >&2
     exit 1
 fi
-curl --cert anarchyirc-client-key.crt --key anarchyirc-client-key.key --data-urlencode cert@"${1}" https://sam.ohnopub.net/~binki/anarchyirc-link-block-service/index.cgi/update
+curl --cert anarchyirc-client-key.crt --key anarchyirc-client-key.key --data-urlencode cert@"${2}" "${1}"/update
